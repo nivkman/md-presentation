c0e50f36ce7efa0b93f353bd0509ae134e0a5b1f
Niv Kaufman <nivkman@gmail.com>
2026-05-03 23:06:29 +0300

    feat: add sample slides.js content

---
+++ slides.js
@@ -0,0 +1,37 @@
+const SLIDES = `
+# Hello, World
+
+This is your first slide.
+
+---
+
+## Code Example
+
+Inline \`code\` looks great.
+
+\`\`\`
+function greet(name) {
+  return "Hello, " + name;
+}
+\`\`\`
+
+---
+
+## A List
+
+- First item
+- Second item
+- Third item
+
+---
+
+## *Italic* and **Bold**
+
+Use emphasis to **highlight** what *matters*.
+
+---
+
+## The End
+
+Thanks for watching.
+`;
